81718a7086a2580c1f1e8c87c9b588e560b192f3
Cristinel <druta.cristin@gmail.com>
2024-10-03 15:46:37 +0300

    Student table changes

--- database/migrations/2024_07_16_112701_create_students_table.php
+++ database/migrations/2024_07_16_112701_create_students_table.php
@@ -14,11 +14,21 @@ return new class extends Migration
         if (!Schema::hasTable('students')) {
             Schema::create('students', function (Blueprint $table) {
                 $table->id();
-                $table->foreignId('user_id')->constrained()->onDelete('cascade');
+                $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
                 $table->string('full_name');
+                $table->string('student_number');
                 $table->string('email')->nullable();
-                $table->string('group');
-                $table->string('series');
+                $table->foreignId('group')->constrained('groups')->onDelete('cascade');
+                $table->foreignId('series')->constrained('series')->onDelete('cascade');
+                $table->integer('year');
+                $table->integer('semester');
+                $table->string('faculty');
+                $table->string('specialization');
+                $table->date('date_of_birth')->nullable();
+                $table->string('birth_place')->nullable();
+                $table->string('address')->nullable();
+                $table->string('city')->nullable();
+                $table->string('phone')->nullable();
                 $table->timestamps();
             });
         };
